ddb8c88e5bfb427d132ee0b8666893a57c377ec2
MuriloSMI <149427808+MuriloSMI@users.noreply.github.com>
2025-12-12 08:40:57 -0300

    Update route.js

--- app/noticias/route.js
+++ app/noticias/route.js
@@ -1,19 +1,23 @@
 import { NextResponse } from 'next/server';
-import clientPromise from '../../../lib/mongodb';
+import clientPromise from '../../lib/mongodb';
 
+/**
+ * @returns {NextResponse}
+ */
 export async function GET() {
     try {
         const client = await clientPromise;
         const db = client.db('portalnoticias');
-        
+
         const noticias = await db.collection('noticias').find({}).toArray();
 
         return NextResponse.json(noticias, { status: 200 });
-        
+
     } catch (e) {
-        console.error("Erro ao retornar a collection na rota /noticias:", e);
+        console.error("Erro ao retornar a collection pura na rota /noticias:", e);
+
         return NextResponse.json(
-            { error: "Falha ao acessar o banco de dados de notícias." }, 
+            { error: "Falha interna ao acessar o banco de dados de notícias." },
             { status: 500 }
         );
     }

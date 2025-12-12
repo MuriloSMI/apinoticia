fb7eb3d4f54020b8de20966d921c56e91ec644e7
MuriloSMI <149427808+MuriloSMI@users.noreply.github.com>
2025-12-12 08:47:57 -0300

    Update route.js

--- app/noticias/route.js
+++ app/noticias/route.js
@@ -1,23 +1,22 @@
 import { NextResponse } from 'next/server';
-import clientPromise from '../../lib/mongodb';
+import dbConnect from '../../lib/dbConnect'; 
+import Noticia from '../../models/Noticia'; 
 
 /**
  * @returns {NextResponse}
  */
 export async function GET() {
     try {
-        const client = await clientPromise;
-        const db = client.db('portalnoticias');
+        await dbConnect(); 
 
-        const noticias = await db.collection('noticias').find({}).toArray();
+        const noticias = await Noticia.find({}).exec();
 
         return NextResponse.json(noticias, { status: 200 });
-
+        
     } catch (e) {
-        console.error("Erro ao retornar a collection pura na rota /noticias:", e);
-
+        console.error("Erro ao retornar a collection na rota /noticias:", e);
         return NextResponse.json(
-            { error: "Falha interna ao acessar o banco de dados de notícias." },
+            { error: "Falha interna ao acessar o banco de dados (Mongoose)." }, 
             { status: 500 }
         );
     }
